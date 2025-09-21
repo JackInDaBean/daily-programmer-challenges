13f2d7120f010f624f381b58612fe8f484977517
Jake Isaac Harvey <jake.harvey115@gmail.com>
2025-09-21 12:19:15 +1200

    Update DailyProgrammerChallenge#6.cpp

--- DailyProgrammerChallenge#6.cpp
+++ DailyProgrammerChallenge#6.cpp
@@ -20,24 +20,29 @@ int main()
 	double piSum = 0.0;
 
 	// For loop runs for 5 billion iterations as this many are required to calculate accurately to 30 decimals - lowering this gives pi accurately up to x amount of decimals
-
-	for (int i = 1; i < 5000000000; i++) // Set i to equal 1 so that I can perform the % on i to check for positive or negative
+	
+	// Set i to equal 1 so that I can perform the % on i to check for positive or negative
+	for (int i = 1; i < 5000000000; i++)
 	{
 		currentCalculation = (Numerator / Denominator);
 		Denominator += 2;
-
-		if (i == 1) { // If the index number is 1 e.g. starting number then add current calculation and skip straight to the next iteration
+		
+		// If the index number is 1 e.g. starting number then add current calculation and skip straight to the next iteration
+		if (i == 1) {
 			piSum = currentCalculation;
 			continue; 
 		}
-
-		else if (i % 2 == 0) { // If the index number is positive then subtract the previous calculation.
+			
+		// If the index number is positive then subtract the previous calculation.
+		else if (i % 2 == 0) {
 			piSum -= currentCalculation;
 		}
-
-		else if (i % 2 != 0) { // If the index number is negative then add the previous calculation.
+			
+		// If the index number is negative then add the previous calculation.
+		else if (i % 2 != 0) { 
 			piSum += currentCalculation;
 		}
 	}
 	std::cout << std::fixed << std::setprecision(30) << piSum;
 }
+
